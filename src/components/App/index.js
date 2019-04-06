97e655c7b6dd6b07296aa1fa1c916262e4aa41bc
Rachel Sleet <rachelsleet@gmail.com>
2019-04-06 20:37:35 +0200

    navigation bar and simple routing implemented

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router,
+         Route,
+         } from 'react-router-dom';
+
 import './App.css';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 const googleAPI = "https://www.googleapis.com/books/v1/volumes?langRestrict=en&q=";
 const stockImage = "https://images.unsplash.com/photo-1549758225-5835373bcbc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=738&q=80";
@@ -22,6 +37,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <Navigation />
+
+          <hr />
+
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          
+        </Router>
+
         <header className="App-header">
           Book Club
         </header>
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUp = () => (
+    <div>
+        <p>SignUp</p>
+    </div>
+);
+
+export default SignUp;
